c43072b85d86136d03d49bdf95c42c7b4d7b1d40
Ian Miell <ian.miell@gmail.com>
2017-09-26 18:53:52 +0100

    buglets

--- nomad.py
+++ nomad.py
@@ -16,11 +16,11 @@ nomad_client1.login('vagrant ssh nomad2')
 nomad_client2.login('vagrant ssh nomad3')
 
 if root.send_and_get_output('vagrant plugin list | grep landrush') == '':
-	shutit.multisend('vagrant plugin install landrush')
+	shutit.multisend('vagrant plugin install landrush',{'assword':pw})
 
-nomad_server_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad1.vagrant.test | awk '{print $2}'""")
-nomad_client1_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad2.vagrant.test | awk '{print $2}'""")
-nomad_client2_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad3.vagrant.test | awk '{print $2}'""")
+nomad_server_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad1.vagrant.test | awk '{print $2}'""").strip()
+nomad_client1_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad2.vagrant.test | awk '{print $2}'""").strip()
+nomad_client2_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad3.vagrant.test | awk '{print $2}'""").strip()
 
 
 nomad_server.send('curl https://raw.githubusercontent.com/hashicorp/nomad/master/demo/vagrant/server.hcl > server.hcl')
