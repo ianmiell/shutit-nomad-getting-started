9a513013af16e4d74e42fc4a01b4b90b579fe20f
Ian Miell <ian.miell@gmail.com>
2017-09-26 20:30:52 +0100

    caret

--- nomad.py
+++ nomad.py
@@ -1,6 +1,7 @@
 import shutit
 
-# Assumes vagrant, and vagrant landrush are installed
+# Assumes vagrant is installed
+# 'pip install shutit' to install python shutit package
 
 root = shutit.create_session('bash',loglevel='info',echo=True)
 nomad_server = shutit.create_session('bash',loglevel='info')
@@ -18,10 +19,9 @@ nomad_client2.login('vagrant ssh nomad3')
 if root.send_and_get_output('vagrant plugin list | grep landrush') == '':
 	shutit.multisend('vagrant plugin install landrush',{'assword':pw})
 
-nomad_server_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad1.vagrant.test | awk '{print $2}'""").strip()
-nomad_client1_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad2.vagrant.test | awk '{print $2}'""").strip()
-nomad_client2_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w nomad3.vagrant.test | awk '{print $2}'""").strip()
-
+nomad_server_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w ^nomad1.vagrant.test | awk '{print $2}'""").strip()
+nomad_client1_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w ^nomad2.vagrant.test | awk '{print $2}'""").strip()
+nomad_client2_ip = root.send_and_get_output("""vagrant landrush ls 2> /dev/null | grep -w ^nomad3.vagrant.test | awk '{print $2}'""").strip()
 
 nomad_server.send('curl https://raw.githubusercontent.com/hashicorp/nomad/master/demo/vagrant/server.hcl > server.hcl')
 nomad_server.send('sudo nomad agent -config server.hcl',background=True)
